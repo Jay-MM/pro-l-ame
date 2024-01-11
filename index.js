204feb60f2f140e0a3e307aeb2dbd202ea60bc08
Jesus M <marquez.jay444@gmail.com>
2024-01-11 02:50:56 -0600

    reformats qusstion structure

--- index.js
+++ index.js
@@ -14,96 +14,103 @@ const questions = [
   {
     type: 'input',
     name: 'email',
-    message: '\nPlease enter your email address.',
+    message: 'Please enter your email address.\n',
     default: 'marquez.jay444@gmail.com',
   },
   {
     type: 'input',
     name: 'github',
-    message: '\nPlease enter your GitHub username.',
+    message: 'Please enter your GitHub username.\n',
     default: 'Jay-MM',
   },
   { 
     type: "input",
     name:"linkedin",
-    message:"\nWhat is your LinkedIn profile sub-domain?",
+    message:"What is your LinkedIn profile sub-domain?\n",
     default: 'marquez-jesus'
   },
   {
     type: 'input',
     name: 'title',
-    message: '\nWhat is the title of your project?',
+    message: 'What is the title of your project?\n',
   },
   {
     type: 'input',
     name: 'motivation',
-    message: '\nWhat was your motivation?',
+    message: 'What was your motivation?\n',
   },
   {
     type: 'input',
     name: 'buildReason',
-    message: '\nWhy did you build this project?',
+    message: 'Why did you build this project?\n',
   },
   {
     type: 'input',
     name: 'problemSolved',
-    message: '\nWhat problem does it solve?',
+    message: 'What problem does it solve?\n',
   },
   {
     type: 'input',
     name: 'lessonsLearned',
-    message: '\nWhat did you learn?',
+    message: 'What did you learn?\n',
   },
   {
     type: 'editor',
     name: 'installation',
-    message: '\nWhat are the steps required to install your project? \nProvide a step-by-step description of how to get the development environment running.\n',
+    message: 'What are the steps required to install your project? \nProvide a step-by-step description of how to get the development environment running.\n',
   },
   {
     type: 'editor',
     name: 'usage',
-    message: '\nProvide instructions and examples for use. \nInclude screenshots as needed by:\n (1)creating an `assets/images` folder in your repo\n (2)upload your screenshot to it.\n (3)using the relative filepath, add it to your README using the following syntax:\n\n ![alt text](assets/images/screenshot.png)'
+    message: 'Provide instructions and examples for use. \nInclude screenshots as needed by:\n (1)creating an `assets/images` folder in your repo\n (2)upload your screenshot to it.\n (3)using the relative filepath, add it to your README using the following syntax:\n ![alt text](assets/images/screenshot.png)\n'
   },
 
   {
     type: 'editor',
     name: 'credits',
-    message: '\nList your collaborators, if any, with links to their GitHub profiles.\nIf you used any third-party assets that require attribution, list the creators with links to their primary web presence.\nIf you followed tutorials, include links.',
+    message: 'List your collaborators, if any, with links to their GitHub profiles.\nIf you used any third-party assets that require attribution, list the creators with links to their primary web presence.\nIf you followed tutorials, include links.\n',
   },
   {
     type: 'list',
     name: 'license',
-    message: '\nChoose a license for your project. If you need help choosing a license, refer to https://choosealicense.com/.',
+    message: 'Choose a license for your project. If you need help choosing a license, refer to https://choosealicense.com/.\n',
     choices: ['MIT' ,'ISC' , 'apache-2.0', 'gpl-3.0', 'none'],
   },
   {
     type: 'confirm',
     name: 'featureConfirm',
-    message: '\nDoes this project have alot of features?',
+    message: 'Does this project have alot of features?',
     default: false,
   },
   {
     type: 'editor',
     name: 'features',
-    message: '\nList all the features here :',
+    message: 'List all the features here :',
     when: (answers) => answers.featureConfirm
+  },
+  {
+    type: 'confirm',
+    name: 'contributeConfirm',
+    message: 'Do you want to add / create any contribution guidelines?',
+    default: false,
   },
   {
     type: 'editor',
     name: 'contribute',
-    message: 'add contribution guidelines here. \nThe Contributor Covenant (https://www.contributor-covenant.org/version/2/1/code_of_conduct/) is an industry standard\nYou can write your own if you prefer.'
+    message: 'Add contribution guidelines here. \n\nThe Contributor Covenant (https://www.contributor-covenant.org/version/2/1/code_of_conduct/) is an industry standard\n\nYou can write your own if you prefer.',
+    when: (answers) => answers.contributeConfirm
   },
 
   {
     type: 'confirm',
     name: 'addTests',
-    message: '\nDid you include any tests?',
+    message: 'Did you include any tests?',
     default: false,
   },
   {
     type: 'editor',
     name: 'tests',
-    message: '\nProvide examples on how to run your tests.',
+    message: 'Provide examples on how to run your tests.',
     when: (answers) => answers.addTests,
   },
   
